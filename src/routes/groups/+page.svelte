7cf1cb11d4e91f9e9035432a1a0654aa146419e4
Suyoun Lee <jenslee705@kaist.ac.kr>
2023-06-11 22:21:33 +0900

    Resolve merge conflicts

--- src/routes/groups/+page.svelte
+++ src/routes/groups/+page.svelte
@@ -1,49 +1,26 @@
 <script lang="ts">
-  import GroupCard from "$lib/components/GroupCard.svelte";
-  import AddButton from "../../lib/components/AddButton.svelte";
+	import GroupCard from "$lib/components/GroupCard.svelte";
+	import AddButton from "../../lib/components/AddButton.svelte";
 
-  import groupData from "../../lib/data/groups.json";
-  import colorData from "../../lib/data/colors.json";
-  import type { Color, Group } from "$lib/types";
+	import groupData from "../../lib/data/groups.json";
+	import colorData from "../../lib/data/colors.json";
+	import type { Color, Group } from "$lib/types";
 
-  const colors: Color[] = colorData;
-  const groups: Group[] = groupData;
+	const colors: Color[] = colorData;
+	const groups: Group[] = groupData;
 
-  const addGroup = () => {
-    console.log("Add Group!");
-  };
+	const addGroup = () => {
+		console.log("Add Group!");
+		window.location.href = "/groups/add";
+	};
 </script>
 
-<div class="center">
-	<div class="form-control">
-		<h1 class="text-xl mb-4 text-center">Group Name:</h1>
-
-		<input
-			type="text"
-			bind:value={groupName}
-			class="input input-bordered w-full"
-		/>
-
 <div class="list-container">
-  {#each groups as group, i}
-    <GroupCard {group} color={colors[i % colors.length]} />
-  {/each}
+	{#each groups as group, i}
+		<GroupCard {group} color={colors[i % colors.length]} />
+	{/each}
 </div>
 <AddButton fixed onClick={addGroup} />
 
 <style>
-	.center {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
-
-	.form-control {
-		width: 300px;
-	}
-
-	.btn {
-		margin-top: 20px;
-	}
 </style>
---
+++ src/routes/groups/add/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let groupName = "";
+
+	function handleClick() {
+		console.log(groupName);
+	}
+</script>
+
+<div class="center">
+	<div class="form-control">
+		<h1 class="text-xl mb-4 text-center">Group Name:</h1>
+
+		<input
+			type="text"
+			bind:value={groupName}
+			class="input input-bordered w-full"
+		/>
+
+		<button class="btn btn-outline w-full" on:click={handleClick}
+			>Enter</button
+		>
+	</div>
+</div>
+
+<style>
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.form-control {
+		width: 300px;
+	}
+
+	.btn {
+		margin-top: 20px;
+	}
+</style>
